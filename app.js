38ccf99ddeba86261d92e4d8e7181af1420b32c1
Git-Support / XCC2 <99868523+frozesentic@users.noreply.github.com>
2023-08-11 19:54:05 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,35 +1,77 @@
-const apiKey = '625805d6ddb0cabe56ac9eafaf18fe37';
-const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
+const container = document.querySelector('.container');
+const search = document.querySelector('.search-box button');
+const weatherBox = document.querySelector('.weather-box');
+const weatherDetails = document.querySelector('.weather-details');
+const error404 = document.querySelector('.not-found');
 
-const weatherInfoContainer = document.getElementById('weather-info');
-const locationInput = document.getElementById('location');
-const getWeatherBtn = document.getElementById('get-weather-btn');
+search.addEventListener('click', () => {
 
-async function getWeather(city) {
-    try {
-        const response = await fetch(`${apiUrl}?q=${city}&units=metric&appid=${apiKey}`);
-        const data = await response.json();
+    const APIKey = '625805d6ddb0cabe56ac9eafaf18fe37';
+    const city = document.querySelector('.search-box input').value;
 
-        const weatherData = `
-            <h2>${data.weather[0].description}</h2>
-            <p>Temperature: ${data.main.temp} °C</p>
-            <p>Feels Like: ${data.main.feels_like} °C</p>
-            <p>Pressure: ${data.main.pressure} hPa</p>
-            <p>Humidity: ${data.main.humidity}%</p>
-            <p>Wind Speed: ${data.wind.speed} m/s</p>
-        `;
+    if (city === '')
+        return;
 
-        weatherInfoContainer.innerHTML = weatherData;
-    } catch (error) {
-        console.error('Error fetching weather:', error);
-    }
-}
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
+        .then(response => response.json())
+        .then(json => {
 
-getWeatherBtn.addEventListener('click', () => {
-    const userLocation = locationInput.value;
-    if (userLocation) {
-        getWeather(userLocation);
-    } else {
-        alert('Please enter a valid location.');
-    }
+            if (json.cod === '404') {
+                container.style.height = '400px';
+                weatherBox.style.display = 'none';
+                weatherDetails.style.display = 'none';
+                error404.style.display = 'block';
+                error404.classList.add('fadeIn');
+                return;
+            }
+
+            error404.style.display = 'none';
+            error404.classList.remove('fadeIn');
+
+            const image = document.querySelector('.weather-box img');
+            const temperature = document.querySelector('.weather-box .temperature');
+            const description = document.querySelector('.weather-box .description');
+            const humidity = document.querySelector('.weather-details .humidity span');
+            const wind = document.querySelector('.weather-details .wind span');
+
+            switch (json.weather[0].main) {
+                case 'Clear':
+                    image.src = 'images/clear.png';
+                    break;
+
+                case 'Rain':
+                    image.src = 'images/rain.png';
+                    break;
+
+                case 'Snow':
+                    image.src = 'images/snow.png';
+                    break;
+
+                case 'Clouds':
+                    image.src = 'images/cloud.png';
+                    break;
+
+                case 'Haze':
+                    image.src = 'images/mist.png';
+                    break;
+
+                default:
+                    image.src = '';
+            }
+
+            temperature.innerHTML = `${parseInt(json.main.temp)}<span>°C</span>`;
+            description.innerHTML = `${json.weather[0].description}`;
+            humidity.innerHTML = `${json.main.humidity}%`;
+            wind.innerHTML = `${parseInt(json.wind.speed)}Km/h`;
+
+            weatherBox.style.display = '';
+            weatherDetails.style.display = '';
+            weatherBox.classList.add('fadeIn');
+            weatherDetails.classList.add('fadeIn');
+            container.style.height = '590px';
+
+
+        });
+
+
 });
